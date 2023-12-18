5f51c6b67e0b3c56591e35cd02dc2174f1256466
Kapish Singh <59023093+kapish33@users.noreply.github.com>
2023-12-18 21:49:46 +0530

    country

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+// middleware.ts
+import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
+const RESTRICTED_COUNTRIES = ["PH", "US"];
+export async function middleware(request: NextRequest, _next: NextFetchEvent) {
+  const res = NextResponse.next();
+  const country = request.geo?.country ?? "";
+  console.log("country",country)
+  if (RESTRICTED_COUNTRIES.includes(country)) {
+    return NextResponse.rewrite(new URL("/restricted", request.url));
+  }
+  return res;
+}

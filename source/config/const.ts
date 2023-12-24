285a6b146b73739c9103d4f646b08c1d52d77368
Kapish Singh <59023093+kapish33@users.noreply.github.com>
2023-12-24 21:42:37 +0530

    adding preview of stripe

--- source/config/const.ts
+++ source/config/const.ts
@@ -1,4 +1,10 @@
 export class Const {
   static RAZORPAY_KEY_SECRET = process.env.RAZORPAY_KEY_SECRET;
   static RAZORPAY_KEY_ID = process.env.RAZORPAY_KEY_ID;
+  static STRIPE = {
+    PUBLISHABLE_KEY:
+      "pk_test_51OLS7nSC25q0wv3OaDmOxhA18P0KdfButoVPUrqH45gstzXwqc3XD9I3Gw3YMMBKGME8Ctyj5SH5Zsdpzje2FSpz00fSl42F0Y",
+    SECRET_KEY:
+      "sk_test_51OLS7nSC25q0wv3O4TaeOJMDgtHTfxOYKr86PIU6P0vNpPNaJcwsmje8lwGSUXo6ngJyPDjFF8j7V9VmqECuy37t00qUdISi6N",
+  };
 }
